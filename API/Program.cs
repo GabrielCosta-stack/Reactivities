d90e3f3c98cce03ed13131eda10fe84b50e3e6bc
Gabriel Costa <costa.gabriel.gabriel@gmail.com>
2023-02-03 09:51:00 +0000

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        // Allow any HTTP method
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
 
 
 var app = builder.Build();
@@ -23,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
